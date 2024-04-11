ff19fc71b1edf5fa191d9e36a69915461b4ebe7b
Anjeyo Mike <mike.matekeya@student.moringaschool.com>
2024-04-11 23:29:12 +0300

    javascript event events

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-function addingEventListener() {
+function addingEventListener() {message ('I like your message!')
 }
+function addingEventListener() {
+    const input = document.getElementById('myButton');
+    
+    if (button) {
+        button.addEventListener('click', function() {
+            alert('Button clicked!');
+        });
+    } else {
+        console.error('Button element not found');
+    }
+}
+
+// Call the outer function to execute addEventListener() and activate the event listener
+addingEventListener();
+
+/*function addingEventListener(mybutton) {
+    const button = document.getElementById('myButton');
+    
+    if (button) {
+        button.addEventListener('click', function() {
+            alert('Button clicked!');
+        });
+    } else {
+        console.error('Button element not found');
+    }
+}
+
+// Call the outer function to execute addEventListener() and activate the event listener
+addingEventListener()*/
